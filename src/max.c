00084bb5ca6a3eafe6f41983359833426d6053d4
Tumberjon Rubi <tumberjr@ev-d1.kzn.21-school.ru>
2023-03-08 21:09:27 +0300

    fix 4

--- src/max.c
+++ src/max.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 double biggest(double x, double y)  
 {  
     return( x>y?x:y );  
@@ -8,5 +8,20 @@ int main(){
     double x, y;
     scanf("%lf %lf",&x, &y);
     printf("biggest is %lf, lowest is %lf\n", y, x);
+    return 0;
+}
+*/
+#include <stdio.h>
+double biggest(double x, double y)  
+{  
+    return( x>y?x:y );  
+}  
+
+int main(){
+    double x, y;
+    scanf("%lf %lf",&x, &y);
+    if (x == (int)x && y == (int)y) {
+    printf("biggest is %lf, lowest is %lf\n", x, y);}
+    else printf("n/a\n");
     return 0;
 }
