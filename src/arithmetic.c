c958d1066cf208f2f4ba0b703ab39acb0ffffb40
Tumberjon Rubi <tumberjr@ev-d1.kzn.21-school.ru>
2023-03-08 20:13:59 +0300

    done 3

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
-int sum(int a, int b);
-
-int main()
-{
+int main(){
+    double x, y;
+    scanf("%lf %lf",&x, &y);
+    if (x == (int)x && y == (int)y) {
+        printf("%.0lf %.0lf %.0lf", x+y, x-y, x*y);
+        if (y == 0)
+            printf("n/a\n");
+        else 
+            printf("%d\n", (int)x / (int)y);
+    } else
+        printf("n/a\n");
     return 0;
-}
-
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
+    
 }
